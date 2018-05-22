7f0dbb326b88b097b06b3d8a22441a3227fc0994
Anthony McShane <a89th@hotmail.com>
2018-05-22 15:51:05 +0100

    new task form working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,26 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 get "tasks", to: "tasks#index"
+
+
+get "tasks/new", to: "tasks#new"
+post "tasks", to: "tasks#create"
+
+get "tasks/:id", to: "tasks#show", as: "task"
+
+
 end
