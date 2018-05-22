0afb0746f502946db1c51670fa8bbafd4754c4a6
Anthony McShane <a89th@hotmail.com>
2018-05-22 16:10:39 +0100

    edit task complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
 get "tasks", to: "tasks#index"
 
-
 get "tasks/new", to: "tasks#new"
 post "tasks", to: "tasks#create"
 
-get "tasks/:id", to: "tasks#show", as: "task"
+get "tasks/:id/edit", to: "tasks#edit"
+patch "tasks/:id", to: "tasks#update"
 
+get "tasks/:id", to: "tasks#show", as: "task"
 
 end
